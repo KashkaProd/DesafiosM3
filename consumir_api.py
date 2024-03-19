35e4dd47edd1cd254c16aada02d4223c5fede680
KashkaProd <157856261+KashkaProd@users.noreply.github.com>
2024-03-19 00:20:32 -0300

    Delete consumir_api.py

--- consumir_api.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-url = "https://aves.ninjas.cl/api/birds"
-
-def request_json(url):
-    return json.loads(requests.get(url).text)
-
-data = request_json('https://aves.ninjas.cl/api/birds')
-#print(data)
